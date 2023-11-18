3ae30ff37f637040b742adcaa2b363f0ab6c660f
FurqanEmam <furqanemam@gmail.com>
2023-11-18 16:36:15 +0600

    env inclueded

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-
-// user credential
-// coffee-store
-// R4T6fhIGWMLLne2N
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://coffee-store:R4T6fhIGWMLLne2N@cluster0.sx1hyuo.mongodb.net/?retryWrites=true&w=majority";
+// console.log(process.env.DB_USER, process.env.DB_PASS);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sx1hyuo.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
